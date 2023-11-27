a0cc39a06b8a085ed60b34e38f3e2bb56b28937d
Web | Mobile Developer <110653349+Apollonstarlet@users.noreply.github.com>
2023-11-27 14:28:48 +0300

    update commit

--- lib/CustomUI/AvtarCard.dart
+++ lib/CustomUI/AvtarCard.dart
@@ -13,33 +13,6 @@ class AvatarCard extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
-          Stack(
-            children: [
-              CircleAvatar(
-                radius: 23,
-                child: SvgPicture.asset(
-                  "assets/person.svg",
-                  color: Colors.white,
-                  height: 30,
-                  width: 30,
-                ),
-                backgroundColor: Colors.blueGrey[200],
-              ),
-              Positioned(
-                bottom: 0,
-                right: 0,
-                child: CircleAvatar(
-                  backgroundColor: Colors.grey,
-                  radius: 11,
-                  child: Icon(
-                    Icons.clear,
-                    color: Colors.white,
-                    size: 13,
-                  ),
-                ),
-              )
-            ],
-          ),
           SizedBox(
             height: 2,
           ),
